5d47265b58afd826713bc576b7f856d6c557958a
cnaude <cnaude@zebra>
2015-03-19 21:49:49 -0700

    Fix TLS support.

--- src/main/java/com/cnaude/purpleirc/PurpleBot.java
+++ src/main/java/com/cnaude/purpleirc/PurpleBot.java
@@ -209,7 +209,10 @@ public final class PurpleBot {
             plugin.logInfo("Setting IdentPassword ...");
             configBuilder.setNickservPassword(botIdentPassword);
         }
-        if (ssl || tls) {
+        if (tls) {
+            plugin.logInfo("Enabling TLS ...");
+            configBuilder.addCapHandler(new TLSCapHandler());
+        } else if (ssl) {
             UtilSSLSocketFactory socketFactory = new UtilSSLSocketFactory();
             socketFactory.disableDiffieHellman();
             if (trustAllCerts) {
@@ -219,10 +222,6 @@ public final class PurpleBot {
                 plugin.logInfo("Enabling SSL ...");
             }
             configBuilder.setSocketFactory(socketFactory);
-            if (tls) {
-                plugin.logInfo("Enabling TLS ...");
-                configBuilder.addCapHandler(new TLSCapHandler(socketFactory, true));
-            }
         }
         if (charSet.isEmpty()) {
             plugin.logInfo("Using default character set: " + Charset.defaultCharset());

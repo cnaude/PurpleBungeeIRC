d1c58cb308c702a15b24bbff7218bf1f34dea253
cnaude <cnaude@zebra>
2015-05-08 20:17:00 -0700

    Tidy up the error replies.

--- src/main/java/com/cnaude/purpleirc/IRCListeners/ServerResponseListener.java
+++ src/main/java/com/cnaude/purpleirc/IRCListeners/ServerResponseListener.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 cnaude
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.cnaude.purpleirc.IRCListeners;
 
 import com.cnaude.purpleirc.PurpleBot;
@@ -32,21 +48,17 @@ public class ServerResponseListener extends ListenerAdapter {
     @Override
     public void onServerResponse(ServerResponseEvent event) {
         int serverReply = event.getCode();
-        
-        if (serverReply == ReplyConstants.ERR_BADCHANNELKEY) {
-            plugin.logInfo("Bad channel password.");
-        }
-        
-        if (serverReply == ReplyConstants.ERR_BANNEDFROMCHAN) {
-            plugin.logInfo("Banned from the channel.");
-        }
-        
+
+        String rawMessage[] = event.getRawLine().split(" ", 5);
+        String target = rawMessage[3];
+
         if (serverReply == ReplyConstants.ERR_NICKNAMEINUSE) {
-            plugin.logInfo("Nickname already in use.");
+            plugin.logInfo("[" + target + "] Nick is already in use.");
             ircBot.altNickChange();
+        } else if (serverReply == ReplyConstants.ERR_BADCHANNELKEY) {
+            plugin.logError("Cannot join " + target + " (Requires keyword)");
+        } else if (serverReply >= 400 && serverReply <= 599) {
+            plugin.logError(event.getRawLine());
         }
-        
-        //plugin.logDebug("Server response: " + event.getRawLine());
-        
     }
 }

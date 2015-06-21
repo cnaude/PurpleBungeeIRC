df3410970d3287556352be2d52a11ee8b9fea8b3
cnaude <cnaude@zebra.lan>
2015-06-20 18:45:27 -0700

    Update IRC away status correctly in /irc list command when user returns.

--- src/main/java/com/cnaude/purpleirc/PurpleBot.java
+++ src/main/java/com/cnaude/purpleirc/PurpleBot.java
@@ -1487,7 +1487,7 @@ public final class PurpleBot {
             String n = user.getNick();
             n = getNickPrefix(user, channel) + n;
             if (user.isAway()) {
-                n = n + ChatColor.GRAY + " | Away";
+                n = n + ChatColor.GRAY + " | Away | " + user.getAwayMessage();
             }
             if (n.equals(bot.getNick())) {
                 n = ChatColor.DARK_PURPLE + n;
